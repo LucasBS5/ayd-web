2becbee2fdc3f003a7f808b2351107c55e7bf520
Lucas Busnelli <lucasbusnelli19@gmail.com>
2025-06-25 02:23:21 -0300

    Mejoras-responsive
    Leer mas, mostrar menos, margenes, padding, etc.

--- js/script.js
+++ js/script.js
@@ -27,3 +27,19 @@ function showSlides(n) {
 const slide = document.getElementById("slide");
 const clone = slide.innerHTML; 
 slide.innerHTML += clone;
+
+
+/*---------------LEER MÁS/MOSTRAR MENOS (MOBILE)---------------*/
+
+function toggleComentario(boton) {
+  const contenedor = boton.closest('.content-box-2');
+  const oculto = contenedor.querySelector('.contenido-oculto');
+
+  if (oculto.style.display === 'block') {
+    oculto.style.display = 'none';
+    boton.textContent = 'Leer más';
+  } else {
+    oculto.style.display = 'block';
+    boton.textContent = 'Mostrar menos';
+  }
+}
